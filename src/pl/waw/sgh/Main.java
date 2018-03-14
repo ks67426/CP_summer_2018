9926dfbe25b11090b4472fe7d990f56692f691a8
ks67426 <karolina.stelmaszek@gmail.com>
2018-03-14 12:52:42 +0100

    Adding the first var

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -3,6 +3,9 @@ package pl.waw.sgh;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello!!");
+        int a = 5;
+        System.out.println("a="+a);
     }
 }
